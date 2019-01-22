54862232685241377ceec36421f5aca0786b6946
Jekyll [automated] (from Travis CI) <no-reply@connectbot.org>
2019-01-22 01:19:05 +0000

    Deploy connectbot.org to github.com/connectbot/connectbot.github.io.git:master

--- assets/responsive-4813ee7c5ad9d3c762d1f06215f1b38207218199b5d84761bfdf20925030728a.js
+++
@@ -1,3 +0,0 @@
-window.onload=function(){function e(){-1!=n.className.indexOf("slide-in")?n.className=n.className.replace(" slide-in",""):n.className=n.className+" slide-in"}var n=document.getElementById("sidebar");document.getElementById("sidebar-button").addEventListener("click",e,!1)};
-//# sourceMappingURL=/assets/source-maps/responsive.js.map
-//# sourceURL=_assets/js/responsive.js
